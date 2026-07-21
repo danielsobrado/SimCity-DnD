c6c27245c570034492e58f08b69d8b3ddc6a6675
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-07-21 21:27:23 +0400

    Persist heightfield world data

--- src/editor/WorldDocument.js
+++ src/editor/WorldDocument.js
@@ -1,20 +1,35 @@
-export function createWorldDocument(tileMap, objectMap) {
+function resolveWorldModels(heightFieldOrObjectMap, objectMap) {
+  if (objectMap) {
+    return { heightField: heightFieldOrObjectMap, objectMap };
+  }
+  return { heightField: null, objectMap: heightFieldOrObjectMap };
+}
+
+export function createWorldDocument(tileMap, heightFieldOrObjectMap, objectMap = null) {
+  const models = resolveWorldModels(heightFieldOrObjectMap, objectMap);
   return {
     ...tileMap.toDocument(),
-    objects: objectMap.toDocument(),
+    ...(models.heightField ? { heightfield: models.heightField.toDocument() } : {}),
+    objects: models.objectMap.toDocument(),
   };
 }
 
-export function loadWorldDocument(document, tileMap, objectMap) {
+export function loadWorldDocument(document, tileMap, heightFieldOrObjectMap, objectMap = null) {
+  const models = resolveWorldModels(heightFieldOrObjectMap, objectMap);
   const previousTiles = new Uint8Array(tileMap.tiles);
-  const previousObjects = objectMap.toDocument();
+  const previousHeights = models.heightField ? new Float32Array(models.heightField.heights) : null;
+  const previousObjects = models.objectMap.toDocument();
 
   try {
     tileMap.loadDocument(document);
-    objectMap.loadDocument(document.objects ?? []);
+    models.heightField?.loadDocument(document.heightfield);
+    models.objectMap.loadDocument(document.objects ?? []);
   } catch (error) {
     tileMap.replaceTiles(previousTiles);
-    objectMap.replaceAll(previousObjects);
+    if (models.heightField && previousHeights) {
+      models.heightField.replaceHeights(previousHeights);
+    }
+    models.objectMap.replaceAll(previousObjects);
     throw error;
   }
 }
