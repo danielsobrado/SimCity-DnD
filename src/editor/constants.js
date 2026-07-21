2065b3582aa44ddd0241e33184b42ad7ac74d340
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-07-21 14:53:26 +0400

    Add editor constants

---
+++ src/editor/constants.js
@@ -0,0 +1,5 @@
+export const MAP_FORMAT_VERSION = 1;
+export const MAX_HISTORY_ENTRIES = 100;
+export const PRIMARY_POINTER_BUTTON = 0;
+export const MINIMAP_SIZE = 192;
+export const PAINT_INTERVAL_MS = 24;
